39c56eaeb861129060f99fcaff3f7054d538297e
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-02-18 09:20:58 +0530

    Create  Invert Binary Tree.java

---
+++ Invert Binary Tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        return root;
+    }
+}
