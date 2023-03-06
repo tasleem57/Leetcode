13f5168ed89201c9cbd866bd9e2bdbfacb551d0e
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-06 08:29:44 +0530

    Create Kth Missing Positive Number.java

---
+++ Kth Missing Positive Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]-(mid+1)>=k)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return low+k;
+    }
+}
