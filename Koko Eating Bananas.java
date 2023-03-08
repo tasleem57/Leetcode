b50229bca3cd57090d369544801dea271a1c4693
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-03-08 09:03:55 +0530

    Create Koko Eating Bananas.java

---
+++ Koko Eating Bananas.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (canEatAll(piles, mid, h)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    
+    private boolean canEatAll(int[] piles, int speed, int h) {
+        int time = 0;
+        for (int pile : piles) {
+            time += (pile - 1) / speed + 1;
+            if (time > h) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
