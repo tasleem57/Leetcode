776298feed3e82a1aa33c99e62d1d509bfa691ba
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-04-21 08:21:31 +0530

    Create Profitable Schemes.java

---
+++ Profitable Schemes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        int len = group.length, MOD = (int)1e9 + 7;
+        int[][][] dp = new int[len + 1][n + 1][minProfit + 1];
+        dp[0][0][0] = 1;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = 0; j <= n; j++) {
+                for (int k = 0; k <= minProfit; k++) {
+                    if (j < members) {
+                        dp[i][j][k] = dp[i - 1][j][k];
+                    } else {
+                        dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][j - members][Math.max(0, k - earn)]) % MOD;
+                    }
+                }
+            }
+        }
+        int sum = 0;
+        for (int j = 0; j <= n; j++) {
+            sum = (sum + dp[len][j][minProfit]) % MOD;
+        }
+        return sum;
+    }
+}
