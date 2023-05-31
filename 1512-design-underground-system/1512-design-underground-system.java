ea4479a392adc2ba7c5a7e799dc47988a0d9b7aa
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-05-31 21:52:42 +0530

    Time: 101 ms (70.17%), Space: 54.2 MB (96.66%) - LeetHub

---
+++ 1512-design-underground-system/1512-design-underground-system.java
@@ -0,0 +1,53 @@
+class UndergroundSystem {
+    private Map<Integer, CheckInInfo> checkIns;
+    private Map<String, TravelInfo> travelTimes;
+
+    public UndergroundSystem() {
+        checkIns = new HashMap<>();
+        travelTimes = new HashMap<>();
+    }
+
+    public void checkIn(int id, String stationName, int t) {
+        checkIns.put(id, new CheckInInfo(stationName, t));
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+        CheckInInfo checkInInfo = checkIns.remove(id);
+        String travel = checkInInfo.stationName + "," + stationName;
+        int travelTime = t - checkInInfo.checkInTime;
+
+        if (travelTimes.containsKey(travel)) {
+            TravelInfo travelInfo = travelTimes.get(travel);
+            travelInfo.totalTime += travelTime;
+            travelInfo.count++;
+        } else {
+            travelTimes.put(travel, new TravelInfo(travelTime, 1));
+        }
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+        String travel = startStation + "," + endStation;
+        TravelInfo travelInfo = travelTimes.get(travel);
+        return (double) travelInfo.totalTime / travelInfo.count;
+    }
+
+    private class CheckInInfo {
+        String stationName;
+        int checkInTime;
+
+        public CheckInInfo(String stationName, int checkInTime) {
+            this.stationName = stationName;
+            this.checkInTime = checkInTime;
+        }
+    }
+
+    private class TravelInfo {
+        int totalTime;
+        int count;
+
+        public TravelInfo(int totalTime, int count) {
+            this.totalTime = totalTime;
+            this.count = count;
+        }
+    }
+}
