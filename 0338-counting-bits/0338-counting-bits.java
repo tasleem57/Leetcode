51260e71940c6e1af9c7f04bdf72d22ac7c9cc92
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-09-01 21:52:23 +0530

    Time: 2 ms (73.73%), Space: 46.4 MB (51.44%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n+1];
+        for (int i = 0; i <= n; i++) {
+            result[i] = Integer.bitCount(i);
+        }
+        return result;
+    }
+}
