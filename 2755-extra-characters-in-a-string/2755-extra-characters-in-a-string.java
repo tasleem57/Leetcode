b9b1ee8a227f8f42cb2c6d79daad2df34b2c1397
TASLEEM BANU S <120388034+tasleem57@users.noreply.github.com>
2023-09-02 21:13:24 +0530

    Time: 30 ms (86.29%), Space: 44.8 MB (8.57%) - LeetHub

---
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    private int[] dp = new int[51]; // Initialize dp array with 0 values
+
+    public int minExtraChar(String s, String[] dictionary) {
+        Arrays.fill(dp, -1); // Initialize dp array with -1 values
+        return minExtraCharHelper(s, dictionary, 0);
+    }
+
+    private int minExtraCharHelper(String s, String[] dictionary, int i) {
+        if (i == s.length()) {
+            return 0;
+        }
+
+        if (dp[i] == -1) {
+            dp[i] = 1 + minExtraCharHelper(s, dictionary, i + 1); // Initialize with one extra character
+
+            for (String w : dictionary) {
+                if (i + w.length() <= s.length() && s.substring(i, i + w.length()).equals(w)) {
+                    dp[i] = Math.min(dp[i], minExtraCharHelper(s, dictionary, i + w.length())); // Update if a word in the dictionary is found
+                }
+            }
+        }
+
+        return dp[i]; // Return the minimum extra characters starting from position i
+    }
+}
